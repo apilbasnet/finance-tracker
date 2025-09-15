d41370cc6067c91a56e391156c314ddb5f9b4a61
apilbasnet <apilbasnet10@gmail.com>
2025-09-15 09:57:45 +0545

    Initial commit

---
+++ apps/financial-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'financial-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/financial-app',
+};
